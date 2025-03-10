15cf79d2209b7d67ba4dbc5e4f9095bac0d0746e
マイ謎会 <mainazoclub@gmail.com>
2025-03-10 22:16:35 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function toggleMenu() {
     document.getElementById("nav-menu").classList.toggle("show");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-container").innerHTML = data;
+
+            // ヘッダーを読み込んだ後にタイトルを変更
+            const pageTitle = document.body.getAttribute("data-title"); // data-titleの値を取得
+            if (pageTitle) {
+                document.getElementById("page-title").textContent = pageTitle;
+            }
+        });
+});
